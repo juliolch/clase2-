367a66c1cdb8a6368204573ad3cdf03ab448cc72
juliolch <102047383+juliolch@users.noreply.github.com>
2025-11-08 14:34:39 -0400

    mas cambios ramita2

--- clase2.py
+++ clase2.py
@@ -2,6 +2,14 @@ class persoa:
     def __init__(self,n):
         self.nombre = n
 
+class estudiante:
+    def __init__(self,n):
+        self.nombre = n
+
+class estudiantes:
+    def __init__(self,n):
+        self.nombre = n
+        
 def function():
     print("xd")
 
